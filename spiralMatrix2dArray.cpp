fe1c1968aeb327c6ff4630c4fa741af5a5f1ea14
satya-no17 <satyarajbarnwal2008@gmail.com>
2025-11-08 14:10:34 +0530

    solved this with half the help of gpt but yeahh i will improve

--- spiralMatrix2dArray.cpp
+++ spiralMatrix2dArray.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int m[4][4];
     int k;
-    for (int i =0;i<4;i++){
-        for (int j =0;j<4;j++){
-        cin>>k;
-        m[i][j]=k;
+    int len = 4;
+    int top = 0, bottom = len - 1, left = 0, right = len - 1;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            cin >> k;
+            m[i][j] = k;
+        }
     }
-    }
-     for (int i =0;i<4;i++){
-        for (int j =0;j<3;j++){
-        
-        cout<<m[i][j]<<" ";
-    }cout<<"\n";
+    for (; top <= bottom && left <= right; top++, bottom--, left++, right--)
+    {
+        for (int j = left; j <= right; j++)
+        {
+            cout << m[top][j] << "  ";
+        }
+        for (int i = top + 1; i <= bottom; i++)
+        {
+            cout << m[i][right] << "  ";
+        }
+        if (top < bottom)
+        {
+            for (int j = right - 1; j >= left; j--)
+            {
+                cout << m[bottom][j] << "  ";
+            }
+        }
+        if (left < right)
+        {
+            for (int i = bottom - 1; i > top; i--)
+            {
+                cout << m[i][left] << "  ";
+            }
+        }
     }
     return 0;
 }
