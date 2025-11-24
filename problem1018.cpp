1807c4beba026f55f59fdfe0ce0b3043d585fb36
satya-no17 <satyarajbarnwal2008@gmail.com>
2025-11-24 23:01:19 +0530

    a little busy today so just tried to solve the leetcode daily challenge but failed as it included the bit manipulation technique so just pushed for streak nothing learned today :(

---
+++ problem1018.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool>ans(nums.size());
+        int num=0;
+        for (int i=0;i<nums.size();i++){
+            num = (num*2 +nums[i])%5;
+            ans[i]= (num==0);
+        }return ans;
+    }
+int main() {
+    vector<int>nums = {}
+    return 0;
+}
